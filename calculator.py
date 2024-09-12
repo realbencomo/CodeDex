460c6e49a2bda715d428b3ce625c584dd52295fc
realbencomo <100797713+realbencomo@users.noreply.github.com>
2024-09-12 11:08:31 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(x, y):
+  answer = x + y
+  return answer
+
+def substract(x, y):
+  answer = x - y
+  return answer
+
+def multiply(x, y):
+  answer = x * y
+  return answer
+
+def divide(x, y):
+  answer = x / y
+  return answer
+
+def exp(x, y):
+  answer = x ** y
+  return answer
+
+print(exp(3, 2))
+print(add(3, 2))
+print(multiply(3, 2))
+print(divide(3, 2))
+print(substract(3, 2))
